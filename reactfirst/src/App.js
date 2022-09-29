e4f7daf6bd2f74ee6a445560823df9d360335f83
Prashant <Prashantraghav145@gmail.com>
2022-09-29 15:28:26 +0530

    react-app

---
+++ reactfirst/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      <label>Enter name</label><input type={"Trushum"}></input>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit Trishum<code>src/App.js</code> and save to reload.
+        </p>
+        
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <label>Enter name</label><input type={"Trushum"}></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
